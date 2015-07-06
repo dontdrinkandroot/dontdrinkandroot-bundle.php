fac376d33204e856d3cda76f38a1971f0f8fe5b0
Philip Washington Sorst <philip@sorst.net>
2015-07-06 22:41:54 +0200

    Adding UuidOrmEntityRepository

--- Listener/Doctrine/AssignUuidListener.php
+++ Listener/Doctrine/AssignUuidListener.php
@@ -6,7 +6,6 @@ namespace Dontdrinkandroot\UtilsBundle\Listener\Doctrine;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Dontdrinkandroot\Entity\UuidEntityInterface;
-use Symfony\Component\Security\Core\Util\SecureRandom;
 
 class AssignUuidListener
 {
@@ -19,36 +18,26 @@ class AssignUuidListener
             /** @var UuidEntityInterface $uuidEntity */
             $uuidEntity = $entity;
             if (null === $uuidEntity->getUuid()) {
-                $uuidEntity->setUuid($this->generateUuid($entityManager));
+                $uuidEntity->setUuid(self::generateUuid());
             }
         }
     }
 
     /**
-     * @param EntityManager $entityManager
+     * Not part of the public API.
      *
      * @return string
      */
-    private function generateUuid(EntityManager $entityManager)
+    private function generateUuid()
     {
-//        $connection = $entityManager->getConnection();
-//        $platform = $connection->getDatabasePlatform();
-//        if (is_a($platform, 'Doctrine\DBAL\Platforms\MySqlPlatform')) {
-//            $statement = $connection->executeQuery('SELECT UUID()');
-//            $uuid = $statement->fetchColumn(0);
-//
-//            return $uuid;
-//        }
-
         $secureRandom = new SecureRandom();
-
+        
         return sprintf(
             '%s-%s-%04x-%04x-%s',
             bin2hex($secureRandom->nextBytes(4)),
             bin2hex($secureRandom->nextBytes(2)),
             mt_rand(0, 0x0fff) | 0x4000,
             mt_rand(0, 0x3fff) | 0x8000,
-
             bin2hex($secureRandom->nextBytes(6))
         );
     }
