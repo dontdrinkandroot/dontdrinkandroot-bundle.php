9d049865f937da365feddcc90c35446e9807abc5
Philip Washington Sorst <philip@sorst.net>
2019-07-14 23:39:06 +0200

    Removing deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ddr_utils');
+        $treeBuilder = new TreeBuilder('ddr_utils');
+        $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
     }
