5a9d44934d5820dbf4b252a1953cfa9f459742ee
Philip Washington Sorst <philip@sorst.net>
2019-09-18 14:53:52 +0200

    Removing root node deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('ddr_utils');
-        $rootNode = $treeBuilder->getRootNode();
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ddr_utils');
 
         return $treeBuilder;
     }
